d4c7399bb08075f13f1832b9994daafb276aa4f6
Roel Bayola <115518524+ynbyl@users.noreply.github.com>
2023-06-21 15:41:08 +0800

    Update coursesV2.py

--- CCC151/coursesV2.py
+++ CCC151/coursesV2.py
@@ -2,6 +2,7 @@ import tkinter as tk
 import sqlite3
 from tkinter import *
 from tkinter import ttk
+import subprocess
 
 
 
@@ -11,7 +12,7 @@ root = Tk()
 root.title("Courses Information Management V2.0")
 root.geometry("600x400")
 root.resizable(False, False)
-my_tree = ttk.Treeview(root)
+course_list = ttk.Treeview(root)
 
 conn = sqlite3.connect("v2.db")
 cursor = conn.cursor()
@@ -26,7 +27,7 @@ cursor.execute('''CREATE TABLE IF NOT EXISTS courses (
 # Function to fetch and display courses
 def rtv():
     # Clear the tree view
-    my_tree.delete(*my_tree.get_children())
+    course_list.delete(*course_list.get_children())
 
     # Fetch courses from the database
     cursor.execute("SELECT * FROM courses")
@@ -34,7 +35,7 @@ def rtv():
 
     # Insert courses into the tree view
     for idx, row in enumerate(rows):
-        my_tree.insert(parent="", index=idx, iid=idx, text=row[0], values=(row[1], row[2]))
+        course_list.insert(parent="", index=idx, iid=idx, text=row[0], values=(row[1], row[2]))
 
 
 # Function to add a course
@@ -58,19 +59,19 @@ def add_course():
 # Function to delete a course
 def delete_course():
     # Get the selected item from the tree view
-    selected_item = my_tree.selection()
+    selected_item = course_list.selection()
     if not selected_item:
         return
 
     # Get the ID of the selected course
-    item_id = my_tree.item(selected_item)["text"]
+    item_id = course_list.item(selected_item)["text"]
 
     # Delete the course from the database using ID
     cursor.execute("DELETE FROM courses WHERE id=?", (item_id,))
     conn.commit()
 
     # Delete the selected course from the tree view
-    my_tree.delete(selected_item)
+    course_list.delete(selected_item)
 
     rtv()
 
@@ -78,15 +79,15 @@ def delete_course():
 # Function to update a course
 def update_course():
     # Get the selected item from the tree view
-    selected_item = my_tree.selection()
+    selected_item = course_list.selection()
     if not selected_item:
         return
 
     # Get the ID of the selected course
-    item_id = my_tree.item(selected_item)["text"]
+    item_id = course_list.item(selected_item)["text"]
 
     # Get the current values of the selected course
-    current_values = my_tree.item(selected_item)["values"]
+    current_values = course_list.item(selected_item)["values"]
 
     # Get the updated values from the entry fields
     coursecode = coursecode_entry.get() or current_values[0]
@@ -105,19 +106,29 @@ def update_course():
     rtv()
 
 
+# Function to OPEN SSISv2.py
+def open_program():
+    program_path = "C:/Users/roelb/PycharmProjects/pythonProject2/ssisv2.py"
+    try:
+        subprocess.Popen(["python", program_path])
+    except FileNotFoundError:
+        print("Program file not found.")
+
+    root.destroy()
 
 
 # Buttons
 add_button = tk.Button(root, text="Add Course", command=add_course)
 add_button.grid(row=2, column=1, padx=5, pady=5)
 
-del_button = tk.Button(root, text="Delete Course", command=delete_course)
-del_button.grid(row=4, column=0, padx=5, pady=5)
+delete_button = tk.Button(root, text="Delete Course", command=delete_course)
+delete_button.grid(row=4, column=0, padx=5, pady=5)
 
-upd_button = tk.Button(root, text="Update", command=update_course)
-upd_button.grid(row=4, column=1, padx=5, pady=5)
+update_button = tk.Button(root, text="Update", command=update_course)
+update_button.grid(row=2, column=0, padx=5, pady=5)
 
-
+ssis_button = tk.Button(root, text="Students", command=open_program)
+ssis_button.grid(row=4, column=1, padx=5, pady=5)
 
 
 # Course Information Form
@@ -135,17 +146,17 @@ coursename_entry.grid(row=1, column=1, padx=5, pady=5)
 
 
 # Treeview
-my_tree = ttk.Treeview(root)
-my_tree["columns"] = ("coursecode", "coursename")
-my_tree.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
+course_list = ttk.Treeview(root)
+course_list["columns"] = ("coursecode", "coursename")
+course_list.grid(row=3, column=0, columnspan=2, padx=5, pady=5)
 
-my_tree.heading("#0", text="ID")
-my_tree.heading("coursecode", text="Course Code")
-my_tree.heading("coursename", text="Course")
+course_list.heading("#0", text="ID")
+course_list.heading("coursecode", text="Course Code")
+course_list.heading("coursename", text="Course")
 
-my_tree.column("#0", width=0, stretch=tk.NO)
-my_tree.column("coursecode", width=150, anchor=tk.CENTER)
-my_tree.column("coursename", width=440, anchor=tk.CENTER)
+course_list.column("#0", width=0, stretch=tk.NO)
+course_list.column("coursecode", width=150, anchor=tk.CENTER)
+course_list.column("coursename", width=440, anchor=tk.CENTER)
 
 
 
